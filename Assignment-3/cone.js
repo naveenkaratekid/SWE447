1d91734e7a2dca6a16c9a59de60b9024e8253703
jacobbird <32081987+jacobbird@users.noreply.github.com>
2018-01-30 14:31:51 -0800

    Cone

---
+++ Assignment-3/cone.js
@@ -0,0 +1,26 @@
+var square = null;
+var gl = null;
+
+function init() {
+	var canvas = document.getElementById("webgl-canvas");
+	gl = WebGLUtils.setupWebGL(canvas);
+	
+
+    if ( !gl ) {
+        alert("Unable to setup WebGL");
+        return;
+    }
+
+    gl.clearColor( 0.0, 1.0, 1.0, 1.0 );
+	
+    cone = new Cone(gl);
+	
+    render();
+}
+
+function render() {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    cone.render();
+}
+
+window.onload = init;
